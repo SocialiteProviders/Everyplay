af9b8b1fa8bf07a968c7f28e0d948ebe7aa7e0c9
DraperStudio <hello@draperstud.io>
2015-03-07 11:59:05 +0100

    Update comments

--- src/EveryplayExtendSocialite.php
+++ src/EveryplayExtendSocialite.php
@@ -6,7 +6,9 @@ use SocialiteProviders\Manager\SocialiteWasCalled;
 class EveryplayExtendSocialite
 {
     /**
-     * Execute the provider.
+     * Register the provider.
+     *
+     * @param \SocialiteProviders\Manager\SocialiteWasCalled $socialiteWasCalled
      */
     public function handle(SocialiteWasCalled $socialiteWasCalled)
     {
